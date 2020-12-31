a1558973a0a5ab66c10e7d5026d93045915bee1b
Pravunath Singh <pravunathsingh@gmail.com>
2020-12-31 10:59:42 +0530

    Added the script.js

--- script.js
+++ script.js
@@ -165,7 +165,7 @@ function getWikiSearchString(searchTerm) {
 
 function getMaxChars() {
     const width = window.innerWidth;
-    let maxChars = 100;
+    let maxChars = 400;
     
     return maxChars;
 }
